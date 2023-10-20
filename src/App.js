93abcd0cb2e8a6dda0c98dd247c290ffd0f9fdb8
s2422980 <y.budnyk@student.utwente.nl>
2023-10-20 20:45:58 +0200

    navbar is implemented with react router now

--- src/App.js
+++ src/App.js
@@ -4,29 +4,18 @@ import Navbar from "./Navbar";
 import Pricing from "./pages/Pricing";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  console.log(window.location);
-  let Component;
-  switch (window.location.pathname) {
-    case "/":
-      Component = Home;
-      break;
-    case "/about":
-      Component = About;
-      break;
-    case "/pricing":
-      Component = Pricing;
-      break;
-    default:
-      Component = Home;
-      break;
-  }
   return (
     <>
       <Navbar />
       <div className="container">
-        <Component />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
     </>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,27 +1,30 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav className="nav">
-      <a href="/" className="site-title">
+      <Link to="/" className="site-title">
         AgencI
-      </a>
+      </Link>
       <ul>
-        <CustomLink href="pricing">Pricing</CustomLink>
-        <CustomLink href="about">About</CustomLink>
+        <CustomLink to="pricing">Pricing</CustomLink>
+        <CustomLink to="about">About</CustomLink>
       </ul>
     </nav>
   );
 }
 
-function CustomLink({ href, children, ...props }) {
-  const path = window.location.pathname;
-  if (path === href) {
-    console.log("they are the same");
-  }
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  //   if (path === to) {
+  //     console.log("they are the same");
+  //   }
   return (
-    <li className={path === href ? "active" : ""}>
-      <a href={href} {...props}>
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
         {children}
-      </a>
+      </Link>
     </li>
   );
 }
